a3650f1fa6a253694896a2ce94555a4ddeea0a38
markjames bersamina <42618215+florbear23@users.noreply.github.com>
2021-10-31 16:21:57 +0800

    Updates

---
+++ precache-manifest.1ac29516318b7abd52d2213297771919.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a54d9bc5d7cb22816953117abf5c6df2",
+    "url": "/florbear23/reactAppv3.git/index.html"
+  },
+  {
+    "revision": "35fec39a725c280eab31",
+    "url": "/florbear23/reactAppv3.git/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/florbear23/reactAppv3.git/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/florbear23/reactAppv3.git/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35fec39a725c280eab31",
+    "url": "/florbear23/reactAppv3.git/static/js/main.94607e4b.chunk.js"
+  },
+  {
+    "revision": "2364d6166df35a4dad79",
+    "url": "/florbear23/reactAppv3.git/static/js/runtime-main.7f500aac.js"
+  }
+]);
